915953edbaebb152679aa4117bdedc5538d48c8b
RafaelLogrado <rafael_logrado@estudante.sesisenai.org.br>
2026-02-23 11:13:31 -0300

    fazendo código principal

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // inicializando o projeto
+import Pizza from "./models/Pizza.js";
+import Receita from "./models/Receita.js";
+
+// 1. usuario define o tamanho da pizza
+const tamanho = new Pizza(35,0.5)
+const pesoPizza = tamanho.getPesoUnitario()
+
+console.log(`O peso da pizza é: ${pesoPizza.toFixed(2)} g`)
+
+// 2. verificando a produção
+const receita = new Receita()
+const qtdeIngredientes = receita.calcularQtdeIngredients()
+const qtdePizza = receita.calcularQtdePizza(pesoPizza)
+
+console.log(`A quantidade de pizza M é: ${qtdePizza}`)
+console.log(`--- Quantidade de Ingredients ---`)
+console.table(qtdeIngredientes)
